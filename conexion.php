105dd93100a72ba15b70b6a26aaa3ef5d49d78c6
natalia-ruby09 <nataliaruby72@gmail.com>
2026-07-24 01:29:10 -0400

    Change database name from 'bd_universidad' to 'bd_practica'

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$database   = "bd_universidad";
+$database   = "bd_practica";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
